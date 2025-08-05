6889a98df4484ec23f641be07290b58211ec4619
Hrob02 <160565963+Hrob02@users.noreply.github.com>
2025-08-05 16:16:38 +1000

    Base Update Movement.h
    Adding doxy and resources

--- Movement.h
+++ Movement.h
@@ -1,1 +1,20 @@
+/**
+@class Movement
+@breif Handles movement execution for autonomous robots.
 
+  Core Functions and Responsibilities:
+    - Movement Execution 
+    - etc
+
+@note
+@author
+@date
+*/
+
+/**
+Resources:
+  - https://github.com/Goodbudy/waitforme/blob/main/issy/src/movementlogic.h - Movement Logic for a single robot given a path
+  - https://github.com/Goodbudy/waitforme/blob/integration_namespace/issy/src/movementlogic.h - Movement logic for multiple robots using unique namespace given a path
+*/
+
+// Begin Code Here
