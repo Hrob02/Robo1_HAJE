94799e5b8918300242bb5e895d7641c5fefe70f1
eleanor low <eleanor.low@outlook.com>
2025-09-16 19:37:41 +1000

    colocon build sucessful

---
+++ install/robo1_haje/share/robo1_haje/firerisk/FireRisk.h
@@ -0,0 +1,20 @@
+/**
+@class Fire Risk
+@breif Given Data about objects within an environment determines a measure of possible fire risk in the area
+
+  Core Functions and Responsibilities:
+    - Accept Data
+    - Perform informed calculations to determine risk
+    - Possible Extension: Inclusion with IOT to use recent eather patterns to adjust risk assessment.
+
+@note
+@author
+@date
+*/
+
+/**
+Resources:
+  - Refer to documentation on teams to plan what calculations would most appropraitly reflect the fire risk for the associated environment
+*/
+
+// Begin Code Here
